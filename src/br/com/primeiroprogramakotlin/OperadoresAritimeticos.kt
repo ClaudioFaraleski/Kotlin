5c7fd20dc5cd76b95d6fad31b5ea4ee414340b0d
ClaudioFaraleski <faraleski@hotmail.com>
2022-06-23 20:13:35 -0300

    Initial commit

---
+++ src/br/com/primeiroprogramakotlin/OperadoresAritimeticos.kt
@@ -0,0 +1,23 @@
+package br.com.primeiroprogramakotlin
+
+fun main() {
+
+    var num1 :Int = 5
+    var num2 :Int = 10
+
+    println(num1 + num2)
+    println(num1.plus(num2))
+    println("==============")
+    println(num1 - num2)
+    println(num1.minus(num2))
+    println("==============")
+    println(num1 * num2)
+    println(num1.times(num2))
+    println("==============")
+    println(num1 / num2)
+    println(num1.div(num2))
+    println("==============")
+    println(num1 % num2)
+    println(num1.mod(num2))
+    println("==============")
+}
